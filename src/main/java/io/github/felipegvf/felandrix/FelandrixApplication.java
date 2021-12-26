ef4019b3d07c69eb49e594e94e143680031ae061
felipe guerra vale da fonseca <fgvfonseca@icloud.com>
2021-12-26 16:57:09 -0300

    feat: Starting up Felandrix project

---
+++ src/main/java/io/github/felipegvf/felandrix/FelandrixApplication.java
@@ -0,0 +1,13 @@
+package io.github.felipegvf.felandrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FelandrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FelandrixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/felipegvf/felandrix/FelandrixApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.felipegvf.felandrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FelandrixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
